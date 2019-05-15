e3c11f1d1c02d20ccac74df2997c566a3bdc55c7
MikeLin <linmichael97@yahoo.com>
2019-05-15 15:17:44 -0400

    Finished full crud routes and functionality to the back end

--- controllers/RestaurantController.js
+++ controllers/RestaurantController.js
@@ -1,12 +1,44 @@
 const Restaurant = require('../models/Restaurant');
 
 const RestaurantController = {
+    index: async(req, res) => {
+        res.json(await Restaurant.find({}));
+    },
     show: async(req, res) => {
         try{
             res.json(await Restaurant.findById(req.params.id));
         } catch(err){
-            console.log(err)
-            res.json(err)
+            console.log(err);
+            res.json(err);
+        }
+    },
+    create: async(req, res) => {
+        try{
+            if(await Restaurant.find({address: req.body.address}) === []){
+                res.status(500).json("Already added this restaurant/address");
+                return;
+            }
+
+            res.json(await Restaurant.create(req.body));
+        } catch(err){
+            console.log(err);
+            res.json(err);
+        }
+    },
+    update: async(req, res) => {
+        try{
+            res.json(await Restaurant.findByIdAndUpdate(req.params.id, req.body, {new: true}));
+        } catch(err){
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    delete: async(req, res) => {
+        try{
+            res.json(await Restaurant.findByIdAndDelete(req.params.id)); 
+        } catch(err){
+            console.log(err);
+            res.status(500).json(err);
         }
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,17 @@ const UserController = require('../controllers/UserController');
 const RestaurantController = require('../controllers/RestaurantController');
 
 //Routes for Users
-router.get('/users/:id', UserController.show);
 router.get('/users', UserController.index);
+router.get('/users/:id', UserController.show);
 router.post('/users', UserController.create);
 router.put('/users/:id', UserController.update);
 router.delete('/users/:id', UserController.delete);
 
 //Routes for Restaurants
+router.get('/restaurants', RestaurantController.index);
 router.get('/restaurants/:id', RestaurantController.show);
+router.post('/restaurants', RestaurantController.create);
+router.put('/restaurants/:id', RestaurantController.update);
+router.delete('/restaurants/:id', RestaurantController.delete);
 
 module.exports = router;
