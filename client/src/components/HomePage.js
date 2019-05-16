4b70f706d0f74e3e3c6d5151cc9b9df6fc71d1b6
MikeLin <linmichael97@yahoo.com>
2019-05-16 12:57:01 -0400

    1. Fixed issue in Homepage where no brackets around JSX 2. Fixed issue in client's package.json where I didn't put dependencies for axios

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -13,7 +13,7 @@ class HomePage extends React.Component {
     render(){
         return(
             <div>
-                !this.state.loggedIn ? <Login changeLoginStatus={this.changeLoginStatus}/> : null
+                {!this.state.loggedIn ? <Login changeLoginStatus={this.changeLoginStatus}/> : null}
             </div>
         )
     }
