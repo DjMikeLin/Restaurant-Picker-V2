fc365f98b72a661e6e9e52f3b6a98433d5919f18
MikeLin <linmichael97@yahoo.com>
2019-05-16 10:18:55 -0400

    1. Updated Login with handleSubmit and handleChange 2. Passed in changeLoginStatus to Login component to trigger when a user has logged in.

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,9 +2,18 @@ import React from 'react';
 import Login from './Login';
 
 class HomePage extends React.Component {
+    state = {
+      loggedIn: false  
+    };
+   
+    changeLoginStatus = () => {
+        this.setState({loggedIn: !this.state.loggedIn});
+        console.log(this.state.loggedIn);
+    }
+ 
     render(){
         return(
-            <Login /> 
+            !this.state.loggedIn ? <Login changeLoginStatus={this.changeLoginStatus}/> : null
         )
     }
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 class Login extends React.Component {
+    state = {
+        loginInfo: {
+            userName: '',
+            password: ''
+        }
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.changeLoginStatus();
+    }
+
+    handleChange = e => {
+        const clonedInfo = {...this.state.loginInfo};
+        clonedInfo[e.target.name] = e.target.value;
+        this.setState({loginInfo: clonedInfo});
+    }
+
     render(){
         return(
-            <form>
-                <input type="text" name="userName" placeholder="User Name" />
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" onChange={this.handleChange} name="userName" placeholder="User Name" />
                 <input type="text" name="password" placeholder="Password" />
                 <button>Login</button>
             </form>            
