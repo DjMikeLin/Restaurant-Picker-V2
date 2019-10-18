c135e92b56dcce655b127a4e18a81935a3b35987
DjMikeLin <linmichael97@yahoo.com>
2019-10-17 20:43:41 -0400

    Finished frontend logic await push to deployment and styling

--- client/src/components/UserView.js
+++ client/src/components/UserView.js
@@ -9,7 +9,15 @@ class UserView extends React.Component {
     state = {
         location: '',
         locationInfo: {
-            name: 'michael' 
+            name: '',
+            categories: [],
+            display_phone: '',
+            is_closed: true,  
+            location: {},
+            price: '',
+            rating: 0,
+            review_count: 0,
+            url: ''
         },
         showInfo: false 
     };     
@@ -29,7 +37,7 @@ class UserView extends React.Component {
    
         let locationInfoCopy = {...this.state.locationInfo};
         locationInfoCopy = (await this.findRestaurant(this.state.location)).data;
-        this.setState({locationInfo: JSON.parse(locationInfoCopy)}); 
+        this.setState({locationInfo: JSON.parse(locationInfoCopy), showInfo: true}); 
     }
 
     render(){
@@ -41,11 +49,30 @@ class UserView extends React.Component {
                     <Button type="submit" variant="success outline-success" size="lg">Find Random Restaurant In Area</Button> 
                 </form>
 
-                <div>
-                    <h1>{this.state.locationInfo.name}</h1>
-                    <h3>{this.state.locationInfo.is_closed}</h3>
-                    <a href={this.state.locationInfo.url}></a>
-                </div>                 
+                {
+                   this.state.showInfo ? 
+                    <div>
+                        <h1>{this.state.locationInfo.name}</h1>
+                        <h3>{this.state.locationInfo.is_closed ? "Closed" : "Open"}</h3>
+                        <h3>Categories</h3>
+                        {
+                            this.state.locationInfo.categories.map(category => <h4>{category.title}</h4>)
+                        }
+                        <h3>{this.state.locationInfo.display_phone}</h3>
+                        <h3>{this.state.locationInfo.price}</h3>
+                        <h3>{this.state.locationInfo.rating}</h3>
+                        <h3>{this.state.locationInfo.review_count}</h3> 
+                        <h3>Location</h3>
+                        <h4>{
+                            this.state.locationInfo.location.address1 + ' ' +
+                            this.state.locationInfo.location.address2 + ' ' +
+                            this.state.locationInfo.location.address3 + ' ' +
+                            this.state.locationInfo.location.city + ' ' +
+                            this.state.locationInfo.location.country        
+                        }</h4>
+                        <a href={this.state.locationInfo.url}>Yelp Link</a>
+                    </div> : null
+                }                 
             </div>
         )
     }
